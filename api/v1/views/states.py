6c04279bdcc7145680f82b15ebf43841c9a42ed4
AdelHamed-X <adelhamed349.ah@gmail.com>
2024-02-26 01:05:58 +0200

    Adding error handlers for the first getter

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -11,8 +11,6 @@ from models.state import State
 def list_states():
     """ Lists all State objects """
     all_states = storage.all(State)
-    if not all_states:
-        abort(404)
     return jsonify([state.to_dict() for state in all_states.values()])
 
 
