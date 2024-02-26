d30d231c9d98d114e4571d0fa1640538575b850e
AdelHamed-X <adelhamed349.ah@gmail.com>
2024-02-26 19:19:45 +0200

    tt2

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -31,7 +31,7 @@ def delete_state_id(state_id):
     state = storage.get(State, state_id)
     if state is None:
         abort(404)
-    state.delete()
+    storage.delete(state)
     storage.save()
     return jsonify({}), 200
 
