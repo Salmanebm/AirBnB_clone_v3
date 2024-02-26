a450622b6347716a7f5df1742e6d4cfae3d82387
AdelHamed-X <adelhamed349.ah@gmail.com>
2024-02-26 19:45:32 +0200

    tt3

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -14,14 +14,12 @@ def list_states():
     return jsonify([state.to_dict() for state in all_states.values()])
 
 
-# @app_views.route('/states/<string:state_id>', methods=['GET'],
-#                  strict_slashes=False)
-# def state_id(state_id):
-#     """ Returns the State object with the given id """
-#     state = storage.get(State, state_id)
-#     if state is None:
-#         abort(404)
-#     return jsonify(state.to_dict())
+@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+def state_id(state_id):
+    """ Returns the State object with the given id """
+    if storage.get(State, state_id) is None:
+        abort(404)
+    return jsonify(storage.get(State, state_id).to_dict())
 
 
 # @app_views.route('states/<state_id>', methods=['DELETE'],
