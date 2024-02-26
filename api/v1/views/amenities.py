9a8ed4d1efcfc567d58d1d99678874808d52a71d
AdelHamed-X <adelhamed349.ah@gmail.com>
2024-02-26 13:15:04 +0200

    Fixing pep8

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -52,7 +52,8 @@ def create_amenity():
     return jsonify(new_amenity.to_dict()), 201
 
 
-@app_views.route('amenities/<amenity_id>', methods=['PUT'], strict_slashes=False)
+@app_views.route('amenities/<amenity_id>', methods=['PUT'],
+                 strict_slashes=False)
 def update_amenity(amenity_id):
     """ Update an existing amenity object """
     amenity = storage.get(Amenity, amenity_id)
