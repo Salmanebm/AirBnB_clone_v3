ce8986e4547a8b8f6d54f728be0da95ee9ba8a13
AdelHamed-X <adelhamed349.ah@gmail.com>
2024-02-26 19:43:34 +0200

    tt3

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -24,46 +24,46 @@ def list_states():
 #     return jsonify(state.to_dict())
 
 
-@app_views.route('states/<state_id>', methods=['DELETE'],
-                 strict_slashes=False)
-def delete_state_id(state_id):
-    """ Deletes an object via its ID """
-    state = storage.get(State, state_id)
-    if state is None:
-        abort(404)
-    state.delete()
-    storage.save()
-    return jsonify({}), 200
+# @app_views.route('states/<state_id>', methods=['DELETE'],
+#                  strict_slashes=False)
+# def delete_state_id(state_id):
+#     """ Deletes an object via its ID """
+#     state = storage.get(State, state_id)
+#     if state is None:
+#         abort(404)
+#     state.delete()
+#     storage.save()
+#     return jsonify({}), 200
 
 
-@app_views.route('/states', methods=['POST'], strict_slashes=False)
-def create_state():
-    """ Creates a new object """
-    header_data = request.get_json()
-    if header_data is None:
-        abort(400, 'Not a JSON')
-    if 'name' not in header_data.keys():
-        abort(400, 'Missing name')
-    new_state = State(**header_data)
-    # storage.new(new_state)
-    new_state.save()
-    return jsonify(new_state.to_dict()), 201
+# @app_views.route('/states', methods=['POST'], strict_slashes=False)
+# def create_state():
+#     """ Creates a new object """
+#     header_data = request.get_json()
+#     if header_data is None:
+#         abort(400, 'Not a JSON')
+#     if 'name' not in header_data.keys():
+#         abort(400, 'Missing name')
+#     new_state = State(**header_data)
+#     # storage.new(new_state)
+#     new_state.save()
+#     return jsonify(new_state.to_dict()), 201
 
 
-@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
-def update_state(state_id):
-    """ Update an existing state object """
-    state = storage.get(State, state_id)
-    if state is None:
-        abort(404)
+# @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
+# def update_state(state_id):
+#     """ Update an existing state object """
+#     state = storage.get(State, state_id)
+#     if state is None:
+#         abort(404)
 
-    header_data = request.get_json()
-    if header_data is None:
-        abort(400, 'Not a JSON')
+#     header_data = request.get_json()
+#     if header_data is None:
+#         abort(400, 'Not a JSON')
 
-    for k, v in header_data.items():
-        if k not in ['id', 'created_at', 'updated_at']:
-            setattr(state, k, v)
+#     for k, v in header_data.items():
+#         if k not in ['id', 'created_at', 'updated_at']:
+#             setattr(state, k, v)
 
-    storage.save()
-    return jsonify(state.to_dict()), 200
+#     storage.save()
+#     return jsonify(state.to_dict()), 200
