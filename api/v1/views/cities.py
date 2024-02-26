f1e4e4daf538f17071b1c2961fa438fa40477076
AdelHamed-X <adelhamed349.ah@gmail.com>
2024-02-26 02:12:03 +0200

    Task 7 - Create a city and link it to an existing state

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -11,7 +11,7 @@ from models.city import City
 @app_views.route('/states/<string:state_id>/cities', methods=['GET'],
                  strict_slashes=False)
 def list_cities(state_id):
-    """ Lists all State objects """
+    """ Lists all City objects """
     if not storage.get(State, state_id):
         abort(404)
     return jsonify([city.to_dict() for city in storage.get(State, state_id).cities])
@@ -20,7 +20,7 @@ def list_cities(state_id):
 @app_views.route('/cities/<string:city_id>', methods=['GET'],
                  strict_slashes=False)
 def city_id(city_id):
-    """ Returns the State object with the given id """
+    """ Returns the City object with the given id """
     city = storage.get(City, city_id)
     if city is None:
         abort(404)
@@ -39,18 +39,21 @@ def delete_city_id(city_id):
     return jsonify({}), 200
 
 
-# @app_views.route('/states', methods=['POST'], strict_slashes=False)
-# def create_state():
-#     """ Creates a new object """
-#     header_data = request.get_json()
-#     if header_data is None:
-#         abort(400, 'Not a JSON')
-#     if 'name' not in header_data.keys():
-#         abort(400, 'Missing name')
-#     new_state = City(**header_data)
-#     storage.new(new_state)
-#     new_state.save()
-#     return jsonify(new_state.to_dict()), 201
+@app_views.route('/states/<state_id>/cities', methods=['POST'], strict_slashes=False)
+def create_city(state_id):
+    """ Creates a new object """
+    if not storage.get(State, state_id):
+        abort(404)
+
+    header_data = request.get_json()
+    if header_data is None:
+        abort(400, 'Not a JSON')
+    if 'name' not in header_data.keys():
+        abort(400, 'Missing name')
+    new_city = City(**header_data, state_id=state_id)
+    storage.new(new_city)
+    new_city.save()
+    return jsonify(new_city.to_dict()), 201
 
 
 # @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
