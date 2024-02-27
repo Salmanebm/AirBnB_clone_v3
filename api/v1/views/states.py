57b49c2b560d83649592b3313da7f1e8a4e697ff
AdelHamed-X <adelhamed349.ah@gmail.com>
2024-02-27 21:22:21 +0200

    Adding json check to put method

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -54,11 +54,10 @@ def update_state(state_id):
     if state is None:
         abort(404)
 
-    header_data = request.get_json()
-    if header_data is None:
+    if request.is_json is None:
         abort(400, 'Not a JSON')
 
-    for k, v in header_data.items():
+    for k, v in request.get_json().items():
         if k not in ['id', 'created_at', 'updated_at']:
             setattr(state, k, v)
 
