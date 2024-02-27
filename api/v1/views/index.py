bcd781e26bac850d6d1e4067c1db704a5621bb6e
AdelHamed-X <adelhamed349.ah@gmail.com>
2024-02-27 21:08:25 +0200

    Changing function names for task 3

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -7,13 +7,13 @@ from models import storage
 
 
 @app_views.route('/status', strict_slashes=False)
-def show_status():
+def show_hbnb_status():
     """ Shows the api response status """
     return jsonify(status="OK")
 
 
 @app_views.route('/stats', strict_slashes=False)
-def show_stats():
+def stats():
     """ Shows the number of each class objects """
     classes = {'amenities': 'Amenity', 'cities': 'City', 'places': 'Place',
                'reviews': 'Review', 'states': 'State', 'users': 'User'}
